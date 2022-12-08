b076bdd4686fc7f7fa5c6dc85843891ec9b42126
cameron-terry <cwterry314@gmail.com>
2022-12-08 06:51:27 -0500

    improve heuristic function

--- js/ai.js
+++ js/ai.js
@@ -14,9 +14,29 @@ class MinimaxNode {
   }
 
   h() {
+    this.board.isMate(); // should set gameOver
     if (this.board.gameOver) {
-      return this.board.side === COLORS.BLACK ? -1000 : 1000;
-    } else return this.board.getMaterialCount();
+      return this.board.side === COLORS.BLACK ? 1000 : -1000;
+    } else {
+      const squares_attacked = this.board.squaresAttacked(true).length;
+
+      const weights = [1, 7, -7, -0.5, 0.5];
+      const heuristics = [
+        this.board.getMaterialCount(),
+        this.board.sqAttacked(this.board.findKingSq(this.board.side ^ 1)),
+        this.board.sqAttacked(this.board.findKingSq(this.board.side)),
+        squares_attacked,
+        this.board.squaresAttacked().length,
+      ];
+
+      let score = 0;
+
+      for (let w in weights) {
+        score += weights[w] * heuristics[w];
+      }
+
+      return score;
+    }
   }
 
   fen() {
@@ -33,20 +53,27 @@ class MinimaxNode {
 }
 
 class MinimaxTree {
-  constructor(boardFen, depth, side) {
+  constructor(boardFen, depth, side, clockStart) {
     let rootBoard = new Board(); // create new board
     rootBoard.boardFromFen(boardFen); // set board to fen
-    rootBoard.side = side; // set side
     rootBoard.createFen(); // set fen
+    rootBoard.side = side; // set side
 
     this.depth = depth; // set depth
     this.root = new MinimaxNode(rootBoard, null); // create root node
+
+    this.nodesBuilt = 1;
+    this.clockStart = clockStart;
+
     this.build(0, this.root); // build tree from root node
   }
 
   build(currentDepth, currentNode) {
     // start at root, continue until depth reached
-    while (currentDepth < this.depth) {
+    while (
+      currentDepth < this.depth &&
+      performance.now() - this.clockStart < 5000
+    ) {
       const newMovesToMakeNodes = currentNode.moves(); // get all moves for current node
 
       // for each move
@@ -61,12 +88,14 @@ class MinimaxTree {
         if (moveMade) {
           newBoard.createFen(); // set fen (so boardFromFen works for children of this move)
           newBoard.side =
-            newBoard.side == COLORS.WHITE ? COLORS.BLACK : COLORS.WHITE;
+            newBoard.side == COLORS.WHITE ? COLORS.BLACK : COLORS.WHITE; // flip turn, since move was made
 
           let newNode = new MinimaxNode(newBoard, move); // create a new node
+          this.nodesBuilt++;
 
           currentNode.children.push(newNode); // add new node to children
-          this.build(currentDepth + 1, newNode); // continue building the tree, starting at the child
+
+          if (!newBoard.gameOver) this.build(currentDepth + 1, newNode); // continue building the tree, starting at the child
         }
       }
 
@@ -76,9 +105,18 @@ class MinimaxTree {
 }
 
 class AI {
-  constructor(board, algorithm = "minimax") {
+  constructor(board, algorithm = "minimax", moves = 2) {
     this.board = board;
     this.algorithm = algorithm;
+    this.depth = moves;
+    this.params = {
+      nodesBuilt: 0,
+      nodesSearched: 0,
+    };
+  }
+
+  setMoves(newMoves) {
+    this.depth = newMoves;
   }
 
   setBoard(newFen) {
@@ -90,53 +128,66 @@ class AI {
     return moves[Math.floor(Math.random() * moves.length)];
   }
 
-  async minimax(node, depth = 0, isMaximizingPlayer, alpha, beta) {
-    if (node.isLeaf()) return [node, node.value];
+  async minimax(node, depth = 0, isMaximizingPlayer, alpha, beta, clockStart) {
+    this.params.nodesSearched++;
 
+    // terminal node
+    if (node.isLeaf() || performance.now() - clockStart > 5000)
+      return [node, node.value];
+
+    // max wants to pick the largest min
     if (isMaximizingPlayer) {
       let bestVal = -Infinity;
       let bestChild = null;
 
+      // each child will get the smallest max
       for (let child_idx in node.children) {
         const [_, value] = await this.minimax(
           node.children[child_idx],
           depth + 1,
           false,
           alpha,
-          beta
+          beta,
+          clockStart
         );
 
+        // new largest min
         if (value > bestVal) {
           bestVal = value;
           bestChild = node.children[child_idx];
         }
 
+        // alpha-beta pruning
         alpha = Math.max(alpha, bestVal);
-
         if (beta <= alpha) break;
       }
 
       return [bestChild, bestVal];
+
+      // min wants to pick the smallest max
     } else {
       let bestVal = Infinity;
       let bestChild = null;
 
+      // each child will get the largest min
       for (let child_idx in node.children) {
         const [_, value] = await this.minimax(
           node.children[child_idx],
           depth + 1,
           true,
           alpha,
-          beta
+          beta,
+          clockStart
         );
 
+        // new smallest max
         if (value < bestVal) {
           bestVal = value;
           bestChild = node.children[child_idx];
         }
 
+        // alpha-beta pruning
         beta = Math.min(beta, bestVal);
-
         if (beta <= alpha) break;
       }
 
@@ -147,25 +198,43 @@ class AI {
   async search() {
     if (this.board.generateMoveList() == 0) return 0;
 
-    if (this.algorithm === "random") {
+    if (this.algorithm === "random" || this.depth == 0) {
       const move = await this.randomMove();
       return move;
     } else if (this.algorithm === "minimax") {
-      const depth = 2;
-      const tree = new MinimaxTree(this.board.fen, depth, this.board.side);
+      const ply = 2 * this.depth; // from experimentation, the algorithm plays better when seeing a move for both sides
 
-      for (let child in tree.root.children) {
-        console.log(tree.root.children[child]);
-      }
+      this.params = {
+        nodesBuilt: 0,
+        nodesSearched: 0,
+      };
 
-      const [bestChild, _] = await this.minimax(
-        tree.root,
-        0,
-        false,
-        -Infinity,
-        Infinity
+      const tree = new MinimaxTree(
+        this.board.fen,
+        ply,
+        this.board.side,
+        performance.now()
       );
 
+      this.params.nodesBuilt = tree.nodesBuilt;
+
+      // display children nodes
+      //   for (let child in tree.root.children) {
+      //     console.log(tree.root.children[child]);
+      //   }
+
+      const [bestChild, bestScore] = await this.minimax(
+        tree.root,
+        0,
+        this.side == COLORS.WHITE,
+        -Infinity,
+        Infinity,
+        performance.now()
+      );
+
+      console.log(
+        `nodes built -> searched at ply ${ply}: ${this.params.nodesBuilt} -> ${this.params.nodesSearched}, eval: ${bestScore}`
+      );
       return bestChild.move;
     }
   }
